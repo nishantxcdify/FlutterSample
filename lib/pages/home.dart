980ec104edbb6e56218af2955a7ee3525264ee53
Nishant Rajpoot <nishant.rajpoot@xcdify.com>
2024-01-21 22:22:02 +0530

    header and textfield added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,25 +7,103 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Breakfast',
-          style: TextStyle(
-              color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-        centerTitle: true,
+        appBar: appBar(),
         backgroundColor: Colors.white,
-        elevation: 0.0,
-        leading: Container(
+        body: Column(
+          children: [_searchField()],
+        ));
+  }
+
+  Container _searchField() {
+    return Container(
+      margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+          color: const Color(0xff1D1617).withOpacity(0.11),
+          blurRadius: 40,
+          spreadRadius: 0.0,
+        ),
+      ]),
+      child: TextField(
+        decoration: InputDecoration(
+            filled: true,
+            hintText: 'Search Pancake',
+            hintStyle: const TextStyle(color: Color(0xffDDDADA), fontSize: 14),
+            fillColor: Colors.white,
+            contentPadding: const EdgeInsets.all(15),
+            prefixIcon: Padding(
+                padding: const EdgeInsets.all(12),
+                child: SvgPicture.asset('assets/icons/Search.svg')),
+            suffixIcon: Container(
+              width: 100,
+              child: IntrinsicHeight(
+                child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                  const VerticalDivider(
+                    color: Colors.black,
+                    indent: 10,
+                    endIndent: 10,
+                    thickness: 0.1,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: SvgPicture.asset('assets/icons/Filter.svg'),
+                  )
+                ]),
+              ),
+            ),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide.none)),
+      ),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+        'Breakfast',
+        style: TextStyle(
+            color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+      ),
+      centerTitle: true,
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      leading: GestureDetector(
+        onTap: () {},
+        child: Container(
           width: 30,
           height: 30,
-          margin: EdgeInsets.all(10),
-          child: SvgPicture.asset('assets/icons/arrow-left.svg'),
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
           decoration: BoxDecoration(
-              color: Color(0xfff7F8F8),
+              color: const Color(0xfff7F8F8),
               borderRadius: BorderRadius.circular(10)),
+          child: SvgPicture.asset(
+            'assets/icons/arrow-left.svg',
+            width: 20,
+            height: 20,
+          ),
         ),
       ),
+      actions: [
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            width: 30,
+            height: 30,
+            margin: const EdgeInsets.all(10),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                color: const Color(0xfff7F8F8),
+                borderRadius: BorderRadius.circular(10)),
+            child: SvgPicture.asset(
+              'assets/icons/dots.svg',
+              width: 5,
+              height: 5,
+            ),
+          ),
+        )
+      ],
     );
   }
 }
